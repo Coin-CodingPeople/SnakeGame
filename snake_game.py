ec24491ec37a7dd5836c4566066d1ad24c982dde
GeonHo Han <hrg921@gmail.com>
2019-01-20 16:30:13 +0900

    add lecture template

--- snake_game.py
+++ snake_game.py
@@ -41,30 +41,16 @@ changeto = ''
 score = 0
 
 
+# Coin:Coding-People - 1. 함수 정의 파트
 # Game Over
 def gameOver():
-    myFont = pygame.font.SysFont('monaco', 72)
-    GOsurf = myFont.render("Game Over", True, red)
-    GOrect = GOsurf.get_rect()
-    GOrect.midtop = (320, 25)
-    playSurface.blit(GOsurf, GOrect)
-    showScore(0)
-    pygame.display.flip()
-    time.sleep(4)
-    pygame.quit()
-    sys.exit()
+    pass
 
 
+# Coin:Coding-People - 1. 함수 정의 파트
 # Show Score
 def showScore(choice=1):
-    SFont = pygame.font.SysFont('monaco', 32)
-    Ssurf = SFont.render("Score  :  {0}".format(score), True, black)
-    Srect = Ssurf.get_rect()
-    if choice == 1:
-        Srect.midtop = (80, 10)
-    else:
-        Srect.midtop = (320, 100)
-    playSurface.blit(Ssurf, Srect)
+    pass
 
 
 while True:
@@ -72,64 +58,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                changeto = 'RIGHT'
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                changeto = 'LEFT'
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                changeto = 'UP'
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                changeto = 'DOWN'
-            if event.key == pygame.K_ESCAPE:
-                pygame.event.post(pygame.event.Event(pygame.QUIT))
+        
+        # Coin:Coding-People - 2. 키보드 조건 파트
 
-    # Validate direction
-    if changeto == 'RIGHT' and direction != 'LEFT':
-        direction = changeto
-    if changeto == 'LEFT' and direction != 'RIGHT':
-        direction = changeto
-    if changeto == 'UP' and direction != 'DOWN':
-        direction = changeto
-    if changeto == 'DOWN' and direction != 'UP':
-        direction = changeto
-
-    # Update snake position
-    if direction == 'RIGHT':
-        snakePos[0] += delta
-    if direction == 'LEFT':
-        snakePos[0] -= delta
-    if direction == 'DOWN':
-        snakePos[1] += delta
-    if direction == 'UP':
-        snakePos[1] -= delta
-
-    # Snake body mechanism
-    snakeBody.insert(0, list(snakePos))
-    if snakePos == foodPos:
-        foodSpawn = False
-        score += 1
-    else:
-        snakeBody.pop()
-    if foodSpawn == False:
-        foodPos = [random.randrange(1, width // 10) * delta, random.randrange(1, height // 10) * delta]
-        foodSpawn = True
+    # Coin:Coding-People - 3. 뱀 움직임 및 먹이 알고리즘
 
     playSurface.fill(white)
     for pos in snakeBody:
         pygame.draw.rect(playSurface, green, pygame.Rect(pos[0], pos[1], delta, delta))
     pygame.draw.rect(playSurface, brown, pygame.Rect(foodPos[0], foodPos[1], delta, delta))
 
-    # Bounds
-    if snakePos[0] >= width or snakePos[0] < 0:
-        gameOver()
-    if snakePos[1] >= height or snakePos[1] < 0:
-        gameOver()
-
-    # Self hit
-    for block in snakeBody[1:]:
-        if snakePos == block:
-            gameOver()
+    # Coin:Coding-People - 4. 종료 조건 파트
+    
     showScore()
     pygame.display.flip()
     fpsController.tick(20)
